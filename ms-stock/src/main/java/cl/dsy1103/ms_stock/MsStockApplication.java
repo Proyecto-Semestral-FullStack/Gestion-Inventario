a86fbf69ed6e71686d913aef3a77600289bcf5ef
devroa <roa.alejandra02@gmail.com>
2026-06-14 20:34:42 -0400

    se modifico el application a formato yml para entornos locales. Y se agrego dependencia de OpenFeign y se adapta la clase principal con la anotacion EnableFeignClients

--- ms-stock/src/main/java/cl/dsy1103/ms_stock/MsStockApplication.java
+++ ms-stock/src/main/java/cl/dsy1103/ms_stock/MsStockApplication.java
@@ -3,15 +3,11 @@ package cl.dsy1103.ms_stock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-/**
- * Clase principal de la aplicación.
- *
- * @SpringBootApplication: Habilita Spring Boot (escanea @Component, @Service, @Repository, etc)
- * @EnableDiscoveryClient: Registra este microservicio en Eureka
- */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MsStockApplication {
 
 	public static void main(String[] args) {
